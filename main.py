969f75212a2c751391e71b951911fc37de442acd
abduganiabduganiyev65-dev <abduganiabduganiyev65@gmail.com>
2026-08-08 23:58:25 +0500

    Update main.py

--- main.py
+++ main.py
@@ -277,7 +277,7 @@ async def run_broadcast(uid, status_msg):
             
             random.shuffle(dialogs)
             for chat_id in dialogs:
-                    try:
+                try:
                     if uid in user_ads:
                         ad = user_ads[uid]
                         if ad["type"] == "photo":
